e295cf47d059ef79f612fd9f264adc19ab4908e4
ashutosh-b-b <43771652+ashutosh-b-b@users.noreply.github.com>
2020-05-31 23:30:26 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-
-
-COMMAND='python3 -u count1.py 2>&1 | tee -a pypy.log'
-TEE='2>&1 | tee -a pypy.log'
-FINAL="$COMMAND $TEE"
-$COMMAND
+#!/usr/local/bin/bash
+STATUSFILE=x.out
+LOGFILE=x.log
+echo $BASH_VERSION
+# (echo 'ok'; echo 'error' >&2)  2> err.log > out.log
+go run main.go out.log &
+(python -u $1) 2> >(tee err.log) > >(tee out.log)
+##Check for bash versions
